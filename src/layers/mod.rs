2237f5b144154c276b8c7898e1d9b0563a2131c1
junyang <mail@junyang.me>
2022-03-15 00:11:05 +0800

    generic sanity modification

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -4,6 +4,6 @@ pub use crate::tensor::*;
 pub use crate::tensor::error::ShapeMismatchError;
 pub type Result<T> = std::result::Result<T, ShapeMismatchError>;
 
-pub trait Layer<InputT: NumT, const INPUT_RANK: usize, OutputT: NumT, const OUTPUT_RANK: usize> {
+pub trait Layer<InputT: NumT, OutputT: NumT, const INPUT_RANK: usize, const OUTPUT_RANK: usize> {
     fn predict(&self, input: &Tensor<InputT, INPUT_RANK>) -> Result<Tensor<OutputT, OUTPUT_RANK>>;
 }
